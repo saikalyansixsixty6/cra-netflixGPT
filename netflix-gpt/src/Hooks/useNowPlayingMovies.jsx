795d3509546aaa111793ad55c23861eda8498ea2
Saikalyan <saikalyan2406@gmail.com>
2023-12-22 21:40:10 +0530

    Tmdb database

--- netflix-gpt/src/Hooks/useNowPlayingMovies.jsx
+++ netflix-gpt/src/Hooks/useNowPlayingMovies.jsx
@@ -14,7 +14,7 @@ const useNowPlayingMovies = () => {
     }
     useEffect(() => {
         getNowPlayingMovies()
-    
+        
       
     }, [])
 
--- netflix-gpt/src/components/Browse.jsx
+++ netflix-gpt/src/components/Browse.jsx
@@ -1,5 +1,7 @@
 import useNowPlayingMovies from "../Hooks/useNowPlayingMovies"
 import Header from "./Header"
+import MainContainer from "./MainContainer"
+import SecondaryContainer from "./SecondaryContainer"
 
 
 const Browse = () => {
@@ -12,7 +14,10 @@ const Browse = () => {
     useNowPlayingMovies()
     return (
       <div>
+        
         <Header/>
+        <MainContainer/>
+        <SecondaryContainer/>
       </div>
     )
 
